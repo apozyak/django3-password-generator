5adaa50eb23f561da7eb7727789b4e355282694e
Andrey Pozyak <apozyak@yandex.ru>
2022-09-30 16:54:38 +0300

    Second commit

--- pass_gen/settings.py
+++ pass_gen/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_es)=7f2c_$uhpx9loqr3ef6s&e)qetk6-*+fq$(dn7)f8t4y7"
+SECRET_KEY = "django-insecure-_es)=7f2c_$usdfsdfsdfhpx9loqr3ef6s&e)qetk6-*+fq$(dn7)f8t4y7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
